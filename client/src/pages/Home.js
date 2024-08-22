d5ed89285d5c7b2ef3371e7e6c57c9171928a4c8
Minh <s3979367@rmit.edu.vn>
2024-08-22 23:26:39 +0700

    added SideNav to test on device's screen

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import { useSession } from '../LoginData';
 import CheckAuth from '../CheckAuth';
+import SideNav from '../components/SideNav';
 
 const Home = () => {
     const { userData, deleteUserData } = useSession();
@@ -11,11 +12,17 @@ const Home = () => {
     }, [checkAuth]);
     
     return (
-        <div>
-            <h1 className='text-red-500'>Home</h1>
-            <p>{userData.username}</p>
-            <button onClick={deleteUserData}>Logout</button>
+        // <div>
+        //     <h1 className='text-red-500'>Home</h1>
+        //     <p>{userData.username}</p>
+        //     <button onClick={deleteUserData}>Logout</button>
 
+        // </div>
+
+        // Implemeting the Home Page
+        <div className='flex w-screen'>
+            <SideNav />
+            
         </div>
     );
 };
