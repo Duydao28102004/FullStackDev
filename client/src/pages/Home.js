36ff4b4a3b8e06a6a08e45dd9dc828bb1cd343cc
Minh <s3979367@rmit.edu.vn>
2024-08-24 00:36:40 +0700

    added UsersFeedPost to test in the main content and set the state to switch between LeftNavbar's IconButton (different IconButton renders different content)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,42 @@
-import React, {useEffect} from 'react';
-import { useSession } from '../LoginData';
+import React, {useEffect, useState} from 'react';
+// import { useSession } from '../LoginData';
 import CheckAuth from '../CheckAuth';
-import SideNav from '../components/SideNav';
+import LeftSideNav from '../components/LeftSideNav';
+import RightSideNav from '../components/RightSideNav';
+import UsersFeedPost from '../components/UsersFeedPost';
 
 const Home = () => {
-    const { userData, deleteUserData } = useSession();
+    // const { userData, deleteUserData } = useSession();
+    const [selectedContent, setSelectedContent] = useState('Home');
 
-    const checkAuth = CheckAuth();
-    useEffect(() => {
-      checkAuth();
-    }, [checkAuth]);
+    const renderMainContent = () => {
+        switch (selectedContent) {
+            case 'Home':
+                return (
+                    <UsersFeedPost 
+                        avatar='/assets/images/test-avatar.jpg'
+                        name='John Doe'
+                        publishedDate='2021-09-15 12:00:00'
+                        content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                        onReactPost={() => {console.log('React')}}
+                        onComment={() => {console.log('Comment')}}
+                        onEdit={() => {console.log('Edit')}}
+                        onDelete={() => {console.log('Delete')}}
+                    />
+                );
+            case 'Friends':
+                return "Friends";
+            case 'User Profile':
+                return "User Profile";
+            default:
+                return <div>Select an option from the left navigation</div>;
+        }
+    };
+
+    // const checkAuth = CheckAuth();
+    // useEffect(() => {
+    //   checkAuth();
+    // }, [checkAuth]);
     
     return (
         // <div>
@@ -20,9 +47,18 @@ const Home = () => {
         // </div>
 
         // Implemeting the Home Page
-        <div className='flex w-screen'>
-            <SideNav />
-            
+        <div className='flex w-full h-screen'>
+            {/* Left-side Navigation */}
+            <LeftSideNav onSelectContent={setSelectedContent}/>
+
+            {/* Main Content */}
+            <div className='flex flex-col w-full h-full py-4 px-8 overflow-y-auto'>
+                {renderMainContent()}
+            </div>
+
+            {/* Right-side Navigation */}
+            <RightSideNav />
+
         </div>
     );
 };
