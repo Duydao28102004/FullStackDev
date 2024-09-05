10a9e1e6e011eb28c60dbaf3f1e20e98921d6038
Minh <s3979367@rmit.edu.vn>
2024-09-05 14:14:18 +0700

    modified styling

--- client/src/components/IconButton.js
+++ client/src/components/IconButton.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function IconButton({ icon, text, onClick, className }) {
     return (
-        <div className={`flex items-center cursor-pointer py-2 px-2 mx-2 ${className}`} onClick={onClick}>
+        <div className={`flex items-center cursor-pointer py-2 px-2 ml-2 ${className}`} onClick={onClick}>
             <img src={icon} alt={`${text} Icon`} className="h-6 w-6" />
             <p className='text-lg font-semibold px-2'>
                 {text}
